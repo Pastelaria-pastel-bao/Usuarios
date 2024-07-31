9598475576d19190d0cd70b86e8fca0c9c9578be
eoMazur <tads.murilomazur@gmail.com>
2024-07-31 19:40:52 -0300

    Feat: spring initializer

---
+++ cadastrousuarios/src/main/java/com/cadastro/usuarios/cadastrousuarios/CadastrousuariosApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.usuarios.cadastrousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrousuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrousuariosApplication.class, args);
+	}
+
+}
