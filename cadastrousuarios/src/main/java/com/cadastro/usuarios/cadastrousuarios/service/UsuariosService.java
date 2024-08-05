6c20f8c60af0b439d28229d0d2222806ea100de9
eoMazur <tads.murilomazur@gmail.com>
2024-08-05 20:02:33 -0300

    Fix: corrigindo getById

--- cadastrousuarios/src/main/java/com/cadastro/usuarios/cadastrousuarios/service/UsuariosService.java
+++ cadastrousuarios/src/main/java/com/cadastro/usuarios/cadastrousuarios/service/UsuariosService.java
@@ -1,6 +1,9 @@
 package com.cadastro.usuarios.cadastrousuarios.service;
 
 import com.cadastro.usuarios.cadastrousuarios.entities.Usuarios;
+import com.cadastro.usuarios.cadastrousuarios.repositories.UsuarioRepository;
+import com.cadastro.usuarios.dtos.UsuariosFindDto;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,14 +24,9 @@ public class UsuariosService {
     @Transactional
     public Usuarios criar(Usuarios usuario) {
         try {
-            if (UsuarioRepository.findByNome(usuario.getNome()).isPresent()) {
-                throw new UsuarioDuplicadoException("Usuario com este nome já existe");
-            }
-            return UsuarioRepository.save(usuario);
-        } catch (UsuarioDuplicadoException | InvalidInputException ex) {
-            log.error("Erro ao criar Usuario: {}", usuario, ex);
-            throw ex;
-            
+
+            return repository.save(usuario);
+        
         } catch (Exception ex) {
             log.error("Erro inesperado ao criar usuario: {}", usuario, ex);
             throw new DatabaseException("Erro no banco de dados");
@@ -36,17 +34,22 @@ public class UsuariosService {
     }
 
     @Transactional
-    public Optional<UsuarioFindDto> getById(Long id){
+    public Optional<UsuariosFindDto> getById(Long id){
         try {
             if (id <= 0) {
                 throw new InvalidInputException("ID inválido");
             }
-            if (!UsuarioRepository.existsById(id)) {
+            if (!repository.existsById(id)) {
                 throw new PastelNaoEncontradoException("Usuario não encontrado");
             }
             ModelMapper mapper = new ModelMapper();
 
-            return mapper.map(Optional.of(UsuarioFindDto),Optional.of(repository.findById(id)));
+            Usuarios usuario = repository.findById(id).get();
+            
+
+            UsuariosFindDto dto = mapper.map(usuario, UsuariosFindDto.class);
+
+            return Optional.of(dto);
 
         } catch (UsuarioNaoEncontradoException | InvalidInputException ex) {
             log.error("Erro ao buscar usuario por ID: {}", id, ex);
