7611abdc6a6cc74d73608de775f483513042265a
Murilo Mazur <158498943+eoMazur@users.noreply.github.com>
2024-08-08 10:39:35 -0300

    Feat: mapeando os endpoints

--- cadastrousuarios/src/main/java/com/cadastro/usuarios/cadastrousuarios/web/UsuarioController.java
+++ cadastrousuarios/src/main/java/com/cadastro/usuarios/cadastrousuarios/web/UsuarioController.java
@@ -5,7 +5,11 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +36,7 @@ public class UsuarioController implements SpringDoc{
     }
 
     @Override
+    @PostMapping
     public ResponseEntity<Usuarios> criar(@Valid @RequestBody Usuarios u) {
         Usuarios usuario = usuarioService.criar(u);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
@@ -39,18 +44,21 @@ public class UsuarioController implements SpringDoc{
 
 
     @Override
+    @GetMapping("/id/{id}")
     public ResponseEntity<Optional<UsuariosFindDto>> getById(@PathVariable Long id) {
         return ResponseEntity.ok(usuarioService.getById(id));
     }
 
 
     @Override
+    @GetMapping("/cpf/{cpf}")
     public ResponseEntity<Optional<UsuariosFindDto>> getByCpf(@PathVariable String cpf) {
         return ResponseEntity.ok(usuarioService.getByCPF(cpf));
     }
 
 
     @Override
+    @DeleteMapping("/id/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         usuarioService.deleteById(id);
         return ResponseEntity.noContent().build();
@@ -58,6 +66,7 @@ public class UsuarioController implements SpringDoc{
 
 
     @Override
+    @PatchMapping("/id/{id}")
     public ResponseEntity<Usuarios> updateUsuarios(@PathVariable Long id, @RequestBody @Valid Usuarios updatedPasteis) {
         Optional<Usuarios> usuario = usuarioService.updateUsuarios(id, updatedPasteis);
         return usuario.map(ResponseEntity::ok)
