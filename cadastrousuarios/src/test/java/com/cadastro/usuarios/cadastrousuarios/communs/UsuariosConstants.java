d9698490b28cdaf2142117612bf01b4680118642
eoMazur <158498943+eoMazur@users.noreply.github.com>
2024-08-09 21:36:58 -0300

    Feat: testes da camada service e correcoes de codigo

--- cadastrousuarios/src/test/java/com/cadastro/usuarios/cadastrousuarios/communs/UsuariosConstants.java
+++ cadastrousuarios/src/test/java/com/cadastro/usuarios/cadastrousuarios/communs/UsuariosConstants.java
@@ -1,11 +1,13 @@
 package com.cadastro.usuarios.cadastrousuarios.communs;
 
 import com.cadastro.usuarios.cadastrousuarios.entities.Usuarios;
+import com.cadastro.usuarios.dtos.UsuariosFindDto;
 
 public class UsuariosConstants {
 
-    public static Usuarios usuarioPadrao = new Usuarios(1L, "Murilo","murilomazur@hotmail.com","11111111111", "42999999999", "Av. Castelo Branco","teste");
-    public static Usuarios usuarioCriar = new Usuarios(null, "Murilo","murilomazur@hotmail.com","11111111111", "42999999999", "Av. Castelo Branco","teste");
+    public static Usuarios usuarioPadrao = new Usuarios(1L, "Murilo","teste@gmail.com","11111111111", "42999999999", "Av. Castelo Branco","teste");
+    public static Usuarios usuarioCriar = new Usuarios(null, "Murilo","teste@gmail.com","11111111111", "42999999999", "Av. Castelo Branco","teste");
     public static Usuarios usuarioNulo = new Usuarios(null, null, null, null, null, null, null);
     public static Usuarios usuarioVazio = new Usuarios();
+    public static UsuariosFindDto usuarioPadraoDto = new UsuariosFindDto(1L, "Murilo","teste@gmail.com","42999999999");
 }
--- cadastrousuarios/src/test/java/com/cadastro/usuarios/cadastrousuarios/service/UsuarioServiceTest.java
+++ cadastrousuarios/src/test/java/com/cadastro/usuarios/cadastrousuarios/service/UsuarioServiceTest.java
@@ -1,20 +1,28 @@
 package com.cadastro.usuarios.cadastrousuarios.service;
 
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import static com.cadastro.usuarios.cadastrousuarios.communs.UsuariosConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
 import com.cadastro.usuarios.cadastrousuarios.entities.Usuarios;
 import com.cadastro.usuarios.cadastrousuarios.exeptions.DatabaseException;
 import com.cadastro.usuarios.cadastrousuarios.exeptions.InvalidInputException;
+import com.cadastro.usuarios.cadastrousuarios.exeptions.UsuarioNaoEncontradoException;
 import com.cadastro.usuarios.cadastrousuarios.repositories.UsuarioRepository;
+import com.cadastro.usuarios.dtos.UsuariosFindDto;
 
 @ExtendWith(MockitoExtension.class)
 public class UsuarioServiceTest {
@@ -44,4 +52,101 @@ public class UsuarioServiceTest {
 
         assertThatThrownBy(() -> service.criar(usuarioVazio)).isInstanceOf(DatabaseException.class);
     }
+
+    @Test
+    public void buscarUsuarioPeloId_ComIdExistente_retornarUsuario(){
+
+        when(repository.existsById(1L)).thenReturn(true);
+        when(repository.findById(1L)).thenReturn(Optional.of(usuarioPadrao));
+
+        Optional<UsuariosFindDto> dto = service.getById(1L);
+
+        assertThat(dto.get().getId()).isEqualTo(1L);
+        assertThat(dto.get().getNome()).isEqualTo("Murilo");
+        assertThat(dto.get().getEmail()).isEqualTo("teste@gmail.com");
+        assertThat(dto.get().getTelefone()).isEqualTo("42999999999");
+
+    }
+
+    @Test
+    public void buscarUsuarioPeloId_ComIdInexistente_lancarException(){ 
+
+        assertThatThrownBy(() -> service.getById(1L)).isInstanceOf(UsuarioNaoEncontradoException.class);
+    }
+
+    @Test
+    public void buscarUsuarioPeloId_ComIdInvalido_lancarException(){
+        assertThatThrownBy(() -> service.getById(-1L)).isInstanceOf(InvalidInputException.class);
+    }
+
+    @Test
+    public void buscarUsuarioPeloCpf_ComCpfValido_retornarUsuario(){
+        when(repository.findByCpf(usuarioPadrao.getCpf())).thenReturn(Optional.of(usuarioPadrao));
+
+        Optional<UsuariosFindDto> dto = service.getByCPF(usuarioPadrao.getCpf());
+
+        assertThat(dto.get().getId()).isEqualTo(1L);
+        assertThat(dto.get().getNome()).isEqualTo("Murilo");
+        assertThat(dto.get().getEmail()).isEqualTo("teste@gmail.com");
+        assertThat(dto.get().getTelefone()).isEqualTo("42999999999");
+    }
+
+    @Test
+    public void buscarUsuarioPeloCpf_ComCpfInexistente_lancarException(){ 
+
+        assertThatThrownBy(() -> service.getByCPF("")).isInstanceOf(InvalidInputException.class);
+        assertThatThrownBy(() -> service.getByCPF(null)).isInstanceOf(InvalidInputException.class);
+    }
+
+    @Test
+    public void buscarUsuarioPeloCpf_ComCpfInvalido_lancarException(){
+        when(repository.findByCpf("1232143223")).thenThrow(UsuarioNaoEncontradoException.class);
+
+        assertThatThrownBy(() -> service.getByCPF("1232143223")).isInstanceOf(UsuarioNaoEncontradoException.class);
+    }
+
+    @Test
+    public void deletarUsuario_ComIdExistente_retornarVoid(){
+
+        when(repository.existsById(1L)).thenReturn(true);
+        assertThatCode(() -> service.deleteById(1L)).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void deletarUsuario_ComIdInexistente_lancarException(){
+
+        assertThatThrownBy(() -> service.deleteById(1L)).isInstanceOf(UsuarioNaoEncontradoException.class);
+    }
+
+    @Test
+    public void deletarUsuario_ComIdInvalido_lancarException(){
+        assertThatThrownBy(() -> service.deleteById(-1L)).isInstanceOf(InvalidInputException.class);
+    }
+
+    @Test
+    public void atualizarUsuario_ComIdExistente_retornarUsuario(){
+        when(repository.existsById(1L)).thenReturn(true);
+        when(repository.save(usuarioPadrao)).thenReturn(usuarioPadrao);
+
+        Optional<Usuarios> dto = service.updateUsuarios(1L, usuarioPadrao);
+
+        assertThat(dto.get().getId()).isEqualTo(1L);
+        assertThat(dto.get().getNome()).isEqualTo("Murilo");
+        assertThat(dto.get().getEmail()).isEqualTo("teste@gmail.com");
+        assertThat(dto.get().getTelefone()).isEqualTo("42999999999");
+    }
+
+    @Test
+    public void atualizarUsuario_ComIdInexistente_lancarException(){
+
+        assertThatThrownBy(() -> service.updateUsuarios(1L, usuarioPadrao)).isInstanceOf(UsuarioNaoEncontradoException.class);
+    }
+
+    @Test
+    public void atualizarUsuario_ComIdIvalido_lancarException(){
+
+        assertThatThrownBy(() -> service.updateUsuarios(-1L, usuarioPadrao)).isInstanceOf(InvalidInputException.class);
+    }
+
+
 }
